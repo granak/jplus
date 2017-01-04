d4b90753f3c33f52e5269cf29239c5159708de7a
marcel <mg.swi@outlook.com>
2017-01-04 08:34:52 +0100

    Definition of Done options init

---
+++ scripts/options/definition-of-done.js
@@ -0,0 +1,18 @@
+var dod_data = {
+    data: [
+        {
+            rapidViewId: 1205,
+            columns: [
+                {
+                    'In Progress': [{
+                        content: 'Acceptance criteria met',
+                        realization: 'mandatory'
+                    }, {
+                        content: '',
+                        realization: 'optional'
+                    }]
+                }
+            ]
+        }
+    ]
+}
