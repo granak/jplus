f65861d6f9798603cfcf9b7fccae37cb2f2d8806
marcel <marcel.granak@solarwinds.com>
2017-07-26 18:26:40 +0200

    [Fixed] Maximum call stack exceeded on right click extend

--- jira-modifications/backlog-right-click-extend.js
+++ jira-modifications/backlog-right-click-extend.js
@@ -2,80 +2,88 @@ if (typeof JPlus == 'undefined') {
     var JPlus = {};
 }
 
-$(document).on('jplus-backlog-is-loaded', function () {
+if (typeof JPlus.PlanContextMenuController == 'undefined') {
     JPlus.PlanContextMenuController = {};
+    JPlus.PlanContextMenuController.extended = false;   // prevent Maximum call stack size exeeded error
+}
 
-    JPlus.PlanContextMenuController._resolveActionsForContext = GH.PlanContextMenuController.resolveActionsForContext;
+$(document).on('jplus-backlog-is-loaded', function () {
+    if (!JPlus.PlanContextMenuController.extended) {
+        JPlus.PlanContextMenuController._resolveActionsForContext = GH.PlanContextMenuController.resolveActionsForContext;
 
-    JPlus.Extend.SetPoints = function (selectedIssues, points, jiraUrl) {
-        JPlus.log("SetPoints called");
-        $.each(selectedIssues, function (index, value) {
-            var jiraApiUrl = jiraUrl + "/rest/api/latest/issue/";
-            var jiraTicket = value;
-            JPlus.info('Ticket to update: ' + value);
-            var jiraUpdateObject = {
-                "update": {
-                    "customfield_10008": [{ "set": points }]
-                }
-            }
-
-            updateJira(jiraApiUrl + jiraTicket, jiraUpdateObject);
-        });
-    }
-
-    GH.PlanContextMenuController.resolveActionsForContext = function (a) {
-        var baseActions = JPlus.PlanContextMenuController._resolveActionsForContext(a);
-        JPlus.Options.Get();
-        JPlus.log('backlog-right-click-extend..resolveActionsForContext called');
-
-        if (JPlus.Options.Data &&
-            JPlus.Options.Data.connection.jiraUrl &&
-            JPlus.Options.Data.connection.jiraUrl.length > 0 &&
-            JPlus.Options.Data.customizations.rightClickActions &&
-            JPlus.Options.Data.customizations.rightClickActions.data &&
-            JPlus.Options.Data.customizations.rightClickActions.data.extendJiraContextMenu) {
-            var extendedStoryPointsActions = [];
-
-            $.each([0.5, 1, 2, 3, 5, 8], function (index, value) {
-                extendedStoryPointsActions.push({
-                    id: "jplus-story-points-" + value,
-                    name: value,
-                    label: value,
-                    actionFn: function () {
-                        JPlus.Extend.SetPoints(a.selectedIssues, value, JPlus.Options.Data.connection.jiraUrl);
+        JPlus.Extend.SetPoints = function (selectedIssues, points, jiraUrl) {
+            JPlus.log("SetPoints called");
+            $.each(selectedIssues, function (index, value) {
+                var jiraApiUrl = jiraUrl + "/rest/api/latest/issue/";
+                var jiraTicket = value;
+                JPlus.info('Ticket to update: ' + value);
+                var jiraUpdateObject = {
+                    "update": {
+                        "customfield_10008": [{ "set": points }]
                     }
-                });
-            });
+                }
 
-            baseActions.push({
-                id: "jplus-story-points",
-                label: "Story points",
-                actions: extendedStoryPointsActions
+                updateJira(jiraApiUrl + jiraTicket, jiraUpdateObject);
             });
-        } else {
-            JPlus.warn('backlog-right-click-extend..resolveActionsForContext - Options' + JPlus.Options.Data);
         }
 
-        return baseActions;
-    };
+        GH.PlanContextMenuController.resolveActionsForContext = function (a) {
+            var baseActions = JPlus.PlanContextMenuController._resolveActionsForContext(a);
+            JPlus.Options.Get();
+            JPlus.log('backlog-right-click-extend..resolveActionsForContext called');
 
-    function updateJira(url, updateObject) {
-        $.ajax({
-            url: url,
-            type: 'PUT',
-            data: JSON.stringify(updateObject),
-            contentType: "application/json",
-            success: function (result) {
-                JPlus.Messages.showSuccessMsg("Action was successfully finished.", {
-                    closeable: true
+            if (JPlus.Options.Data &&
+                JPlus.Options.Data.connection.jiraUrl &&
+                JPlus.Options.Data.connection.jiraUrl.length > 0 &&
+                JPlus.Options.Data.customizations.rightClickActions &&
+                JPlus.Options.Data.customizations.rightClickActions.data &&
+                JPlus.Options.Data.customizations.rightClickActions.data.extendJiraContextMenu) {
+                var extendedStoryPointsActions = [];
+
+                $.each([0.5, 1, 2, 3, 5, 8], function (index, value) {
+                    extendedStoryPointsActions.push({
+                        id: "jplus-story-points-" + value,
+                        name: value,
+                        label: value,
+                        actionFn: function () {
+                            JPlus.Extend.SetPoints(a.selectedIssues, value, JPlus.Options.Data.connection.jiraUrl);
+                        }
+                    });
                 });
-            },
-            error: function (err) {
-                JPlus.Messages.showErrorMsg("Error occured during action. Please try again later.", {
-                    closeable: true
+
+                baseActions.push({
+                    id: "jplus-story-points",
+                    label: "Story points",
+                    actions: extendedStoryPointsActions
                 });
-                JPlus.error(err);
+            } else {
+                JPlus.warn('backlog-right-click-extend..resolveActionsForContext - Options' + JPlus.Options.Data);
             }
-        });
+
+            return baseActions;
+        }
+
+        JPlus.PlanContextMenuController.extended = true;
     }
+
 });
+
+function updateJira(url, updateObject) {
+    $.ajax({
+        url: url,
+        type: 'PUT',
+        data: JSON.stringify(updateObject),
+        contentType: "application/json",
+        success: function (result) {
+            JPlus.Messages.showSuccessMsg("Action was successfully finished.", {
+                closeable: true
+            });
+        },
+        error: function (err) {
+            JPlus.Messages.showErrorMsg("Error occured during action. Please try again later.", {
+                closeable: true
+            });
+            JPlus.error(err);
+        }
+    });
+}
